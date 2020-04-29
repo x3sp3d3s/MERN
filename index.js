28299712b10ae59c849524b0afde980f38d43873
Albert Céspedes Esteve <albertcespedes@gmail.com>
2020-04-29 22:35:54 +0200

    TET
    TETE

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({extended: true}));
 
 // puerto de la app
-const port = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 
 // Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios')); 
@@ -24,6 +24,6 @@ app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas')); 
 
 // arrancar la app
-app.listen(port, '0.0.0.0', () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${PORT}`); 
 })
